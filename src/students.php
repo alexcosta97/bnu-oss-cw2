f276c00b1f8b21d77f5c644658bcac3efa73f839
Alexandre Costa <alex.callum.c@gmail.com>
2018-04-06 14:14:54 +0100

    feat: finish student task

--- src/students.php
+++ src/students.php
@@ -16,11 +16,20 @@
 
     //prepare page content
     $sata['content'] .= "<table border='1'>";
-    $data['content'] .= "<tr><th cols";
-    $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th"
+    $data['content'] .= "<tr><th colspan='9' align='center'>Students</th></tr>";
+    $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Coutnry</th><th>Postcode</th></tr>";
 
+    while($row = mysqli_fetch_array($result)){
+      $data['content'] .= "<tr><td>$row[studentid]</td><td>$row[dob]</td><td>$row[firstname]</td><td>$row[lastname]</td><td>$row[house]</td><td>$row[town]</td><td>$row[county]</td><td>$row[country]</td><td>$row[postcode]</td></tr>";
+    }
+    $data['content'] .= "</table>";
+
+    //render the template
+    echo template("templates/default.php", $data);
   }
   else{
     header("Location: index.php");
   }
-?>
+
+  echo template("templates/partials/footer.php");
+  ?>
