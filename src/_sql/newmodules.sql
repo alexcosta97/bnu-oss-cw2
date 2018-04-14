81e375934e9c326cc0848c20f55b0f4d4e058ab4
Alex <alex.callum.c@gmail.com>
2018-04-14 01:54:25 +0100

    fix: fix database connection and sql errors

--- src/_sql/newmodules.sql
+++ src/_sql/newmodules.sql
@@ -1,4 +1,4 @@
-Insert into 'module'('modulecode', 'name', 'level') values
+Insert into `module`(`modulecode`, `name`, `level`) values
 ('CO158', 'XML', '1'),
 ('CO265', 'Web Services', '2'),
 ('CO366', 'Mobile Systems', '3');
