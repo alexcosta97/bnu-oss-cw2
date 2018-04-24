c4de444c529a4639c37505d7c45ce3b57d754fb7
Alex <alex.callum.c@gmail.com>
2018-04-24 02:17:09 +0100

    feat: add form-group divs to form

--- src/templates/addstudent.php
+++ src/templates/addstudent.php
@@ -1,23 +1,43 @@
 <form action="addstudents.php" method="post">
-  <label for="studentid">Student ID</label>
-  <input type="text" id="studentid" name="studentid"/><br/>
-  <label for="password">Password</label>
-  <input type="password" id="password" name="password"/><br/>
-  <label for="dob">Date Of Birth</label>
-  <input type="date" id="dob" name="dob"/><br/>
-  <label for="firstname">First Name</label>
-  <input type="text" id="firstname" name="firstname"/><br/>
-  <label for="lastname">Last Name</label>
-  <input type="text" id="lastname" name="lastname"/><br/>
-  <label for="house">House</label>
-  <input type="text" id="house" name="house"/><br/>
-  <label for="town">Town</label>
-  <input type="text" id="town" name="town"/><br/>
-  <label for="county">County</label>
-  <input type="text" id="county" name="county"/><br/>
-  <label for="country">Country</label>
-  <input type="text" id="country" name="country"/><br/>
-  <label for="postcode">Postcode</label>
-  <input type="text" id="postcode" name="postcode"/><br/>
+  <div class="form-group">
+    <label for="studentid">Student ID</label>
+    <input type="text" id="studentid" name="studentid"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="dob">Date Of Birth</label>
+    <input type="date" id="dob" name="dob"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="firstname">First Name</label>
+    <input type="text" id="firstname" name="firstname"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="lastname">Last Name</label>
+    <input type="text" id="lastname" name="lastname"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="house">House</label>
+    <input type="text" id="house" name="house"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="town">Town</label>
+    <input type="text" id="town" name="town"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="county">County</label>
+    <input type="text" id="county" name="county"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="country">Country</label>
+    <input type="text" id="country" name="country"/><br/>
+  </div>
+  <div class="form-group">
+    <label for="postcode">Postcode</label>
+    <input type="text" id="postcode" name="postcode"/><br/>
+  </div>
   <input type="submit" value="Save"/>
 </form>
