db3f00eddf71350d37b180e1511ff86c2c00f0cc
Alexandre Costa <alex.callum.c@gmail.com>
2018-04-24 11:57:51 +0100

    feat: style form

--- src/assignmodule.php
+++ src/assignmodule.php
@@ -24,14 +24,18 @@ if (isset($_SESSION['id'])) {
      $sql = "select * from module";
      $result = mysqli_query($conn, $sql);
 
-     $data['content'] .= "<form name='frmassignmodule' action='' method='post' >";
-     $data['content'] .= "Select a module to assign<br/>";
-     $data['content'] .= "<select name='selmodule' >";
+     $data['content'] .= "<h1>Assign Modules</h1>";
+     $data['content'] .= "<form name='frmassignmodule' action='' class='mt-1' method='post' >";
+     $data['content'] .= "<div class='form-row'>";
+     $data['content'] .= "<div class='form-group col-6'";
+     $data['content'] .= "<label for='selmodule'>Select a module to assign</label>";
+     $data['content'] .= "<select name='selmodule' id='selmodule' class='form-control'>";
      // Display the module name sin a drop down selection box
      while($row = mysqli_fetch_array($result)) {
         $data['content'] .= "<option value='$row[modulecode]'>$row[name]</option>";
      }
-     $data['content'] .= "</select><br/>";
+     $data['content'] .= "</select>";
+     $data['content'].= "</div></div>";
      $data['content'] .= "<input type='submit' class='btn btn-primary btn-md' name='confirm' value='Save' />";
      $data['content'] .= "</form>";
    }
