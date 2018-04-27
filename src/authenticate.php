8882fcc2450ce20e24784265beb3b48601294d3b
Alex <alex.callum.c@gmail.com>
2018-04-27 11:27:59 +0100

    feat: apply PDO to authentication

--- src/authenticate.php
+++ src/authenticate.php
@@ -8,17 +8,13 @@
    // If the student has already been authenticated the $_SESSION['id'] variable
    // will been assigned their student id.
 
-
    // redirect to index if $_POST values not set or $_SESSION['id'] is already set
    if (!isset($_POST['txtid']) || !isset($_POST['txtpwd']) || isset($_SESSION['id'])) {
       header("Location: index.php");
 	} else {
-      if (validatelogin($_POST['txtid'],$_POST['txtpwd']) == true) {
-         // valid
+      if (validatelogin($_POST['txtid'], $_POST['txtpwd'])) {
          header("Location: index.php?return=success");
       } else {
-         // invalid
-         unset($_SESSION['id']);
          header("Location: index.php?return=fail");
       }
 	}
