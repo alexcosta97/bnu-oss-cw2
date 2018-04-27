c8b076a428749633f5ae2530f6ac1e78d2e7806f
Alex <alex.callum.c@gmail.com>
2018-04-27 12:05:13 +0100

    feat: apply PDO

--- src/students.php
+++ src/students.php
@@ -10,14 +10,7 @@
     echo template("templates/partials/nav.php");
 
     //Build SQL statement that selects students
-    $sql = "select * from student;";
-
-    $result = mysqli_query($conn,$sql);
-
-    if (!$result) {
-      printf("Error: %s\n", mysqli_error($conn));
-      exit();
-  }
+    $result = $conn -> query("SELECT * FROM student");
 
     //prepare page content
     $data['content'] .= "<form action=
@@ -28,13 +21,14 @@
     $data['content'] .= "<table class='table table-hover table-condensed table-bordered'>";
     $data['content'] .= "<thead class='thead-light'><tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th><th>Selected</th></tr></thead><tbody>";
 
-    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    while($row = $result -> fetch()) {
       $data['content'] .= "<tr><td>$row[studentid]</td><td>$row[dob]</td><td>$row[firstname]</td><td>$row[lastname]</td><td>$row[house]</td><td>$row[town]</td><td>$row[county]</td><td>$row[country]</td><td>$row[postcode]</td>";
       $data['content'] .= "<td>";
       $data['content'] .= "<div class='form-check'>";
       $data['content'] .= "<input type='checkbox' class='form-check-input position-static' name='selected[]' value='$row[studentid]'/>";
       $data['content'] .= "</div></td></tr>";
     }
+    $result -> closeCursor();
     $data['content'] .= "</tbody></table>";
     $data['content'] .= "<br/>";
     $data['content'] .= "<div class='form-group'>";
@@ -42,7 +36,6 @@
     $data['content'] .= "<input class='btn btn-danger btn-md' type='submit' value='Delete'/>";
     $data['content'] .= "</form>";
     $data['content'] .= "</div>";
-    mysqli_free_result($result);
     //render the template
     echo template("templates/default.php", $data);
   }
