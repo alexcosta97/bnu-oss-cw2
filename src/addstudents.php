b1d68cdef81f766fc6be19f849c59e36cd89d950
Alexandre Costa <alex.callum.c@gmail.com>
2018-04-27 16:49:27 +0100

    feat: properly display missing information messages

--- src/addstudents.php
+++ src/addstudents.php
@@ -20,7 +20,7 @@
     //Verifies that all the data has been entered and assigns the entered information into variables
     if(empty($_POST['studentid']))
     {
-      $data['content'] .= "Student ID missing<br/>";
+      $data['validation']['studentid'] = true;
     }
     else{
       $studentid = $_POST['studentid'];
@@ -28,8 +28,7 @@
 
     if(empty($_POST['password']))
     {
-      $data['content'] .= "Password missing<br/>";
-      // $data['validation']['password'] =
+      $data['validation']['password'] = true;
     }
     else
     {
@@ -38,7 +37,7 @@
 
     if(empty($_POST['dob']))
     {
-      $data['content'] .= "Date Of Birth missing</br>";
+      $data['validation']['dob'] = true;
     }
     else{
       $dob = $_POST['dob'];
@@ -46,7 +45,7 @@
 
     if(empty($_POST['firstname']))
     {
-      $data['content'] .= "First Name missing<br/>";
+      $data['validation']['firstname'] = true;
     }
     else
     {
@@ -55,7 +54,7 @@
 
     if(empty($_POST['lastname']))
     {
-      $data['content'] .= "Last Name missing<br/>";
+      $data['validation']['lastname'] = true;
     }
     else{
       $lastname = $_POST['lastname'];
@@ -63,7 +62,7 @@
 
     if(empty($_POST['house']))
     {
-      $data['content'] .= "House missing <br/>";
+      $data['validation']['house'] = true;
     }
     else{
       $house = $_POST['house'];
@@ -71,7 +70,7 @@
 
     if(empty($_POST['town']))
     {
-      $data['content'] .= "Town missing<br/>";
+      $data['validation']['town'] = true;
     }
     else{
       $town = $_POST['town'];
@@ -79,7 +78,7 @@
 
     if(empty($_POST['county']))
     {
-      $data['content'] .= "County missing<br/>";
+      $data['validation']['county'] = true;
     }
     else
     {
@@ -88,7 +87,7 @@
 
     if(empty($_POST['country']))
     {
-      $data['content'] .= "Country missing<br/>";
+      $data['validation']['country'] = true;
     }
     else
     {
@@ -97,7 +96,7 @@
 
     if(empty($_POST['postcode']))
     {
-      $data['content'] .= "Postcode missing<br/>";
+      $data['validation']['postcode'] = true;
     }
     else
     {
@@ -105,12 +104,11 @@
     }
 
     //if any error message has been added to $data['content'] prints the form again with the error messages
-    if(!empty($data['content']))
+    if(!empty($data['validation']))
     {
       echo template("templates/partials/header.php");
       echo template("templates/partials/nav.php");
-      echo template("templates/addstudent.php");
-      echo template("templates/default.php", $data);
+      echo template("templates/addstudent.php", $data);
     }
     //if all the data is present, moves on to add the data to the database
     else{
--- src/templates/addstudent.php
+++ src/templates/addstudent.php
@@ -3,52 +3,132 @@
     <div class="form-group col-6">
       <label for="studentid">Student ID</label>
       <input type="text" id="studentid" class="form-control" name="studentid"/>
+      <?php
+        if(isset($validation['studentid']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a student id number.
+          </div>
+        <?php }
+      ?>
     </div>
     <div class="form-group col-6">
       <label for="password">Password</label>
       <input type="password" id="password" class="form-control" name="password"/>
+      <?php
+        if(isset($validation['password']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a password.
+          </div>
+        <?php }
+      ?>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-6">
       <label for="firstname">First Name</label>
       <input type="text" id="firstname" class="form-control" name="firstname"/>
+      <?php
+        if(isset($validation['firstname']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a First Name.
+          </div>
+        <?php }
+      ?>
     </div>
     <div class="form-group col-6">
       <label for="lastname">Last Name</label>
       <input type="text" id="lastname" class="form-control" name="lastname"/>
+      <?php
+        if(isset($validation['lastname']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a Last Name.
+          </div>
+        <?php }
+      ?>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-6">
       <label for="dob">Date Of Birth</label>
       <input type="date" id="dob" class="form-control" name="dob"/>
+      <?php
+        if(isset($validation['dob']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a Date of Birth.
+          </div>
+        <?php }
+      ?>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-6">
       <label for="house">Number and Street</label>
       <input type="text" id="house" class="form-control" name="house"/>
+      <?php
+        if(isset($validation['house']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a Number and Street.
+          </div>
+        <?php }
+      ?>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-6">
       <label for="town">Town</label>
       <input type="text" id="town" class="form-control" name="town"/>
+      <?php
+        if(isset($validation['town']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a town.
+          </div>
+        <?php }
+      ?>
     </div>
     <div class="form-group col-4">
       <label for="county">County</label>
       <input type="text" id="county" class="form-control" name="county"/>
+      <?php
+        if(isset($validation['county']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a county.
+          </div>
+        <?php }
+      ?>
     </div>
     <div class="form-group col-2">
       <label for="postcode">Postcode</label>
       <input type="text" id="postcode" class="form-control" name="postcode"/>
+      <?php
+        if(isset($validation['postcode']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a postcode.
+          </div>
+        <?php }
+      ?>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-6">
       <label for="country">Country</label>
       <input type="text" id="country" class="form-control" name="country"/>
+      <?php
+        if(isset($validation['country']))
+        {?>
+          <div class="alert alert-danger" role="alert">
+            You need to provide a country.
+          </div>
+        <?php }
+      ?>
     </div>
   </div>
   <input type="submit" class="btn btn-primary btn-md" value="Save"/>
